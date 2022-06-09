d90f59588a0d4b3751fa3db97886c634a37f20fc
Jun Wang <jun.wang@synsense.ai>
2022-06-09 10:38:01 +0800

    implement calc prime in compilation

--- main.cpp
+++ main.cpp
@@ -37,7 +37,8 @@
 // #include "test_basic/virtual_method.h"
 // #include "test_basic/derive_private.h"
 // #include "test_basic/lambda_assign.h"
-#include "test_basic/constexpr.h"
+// #include "test_basic/constexpr.h"
+#include "test_example/calc_prime_in_compilation.h"
 
 int main(int, char**) {
     std::cout << "begin " << std::endl;
@@ -50,7 +51,8 @@ int main(int, char**) {
     // test_noexcept();
     // test_virtual_method();
     // test_derive_private();
-    test_constexpr();
+    // test_constexpr();
+    test_calc_prime();
 
     return 0;
 }
--- test_example/calc_prime_in_compilation.h
+++ test_example/calc_prime_in_compilation.h
@@ -1,18 +1,67 @@
 #ifndef CALC_PRIME_IN_COMPILATION_H_
 #define CALC_PRIME_IN_COMPILATION_H_
 
+#include <iostream>
+#include <utility>
+
 template<size_t... N>
-struct primers{};
+struct Primes {
+    void print() const {
+        (std::cout << ... << (std::to_string(N) + " ")) << '\n';
+    }
+};
 
-primers<2>;
+constexpr bool isPrime(size_t n) {
+    if (n < 2) {
+        return false;
+    }
 
-template<size_t ...N>
-auto next(primers<N...>)
-{
-    return primers<N..., M>{};
+    // std::sqrt 不支持constexpr
+    for (int d = 2; d < n; d++) {
+        if (n % d == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-using primers = decltype(next(...));
+// concat
+template <size_t, typename> struct Cons;
+
+template <size_t T, size_t ...Args>
+struct Cons<T, Primes<Args...>>
+{
+    using type = Primes<T, Args...>;
+};
+
+// filter
+template <size_t...> struct filter;
+
+template <> struct filter<> { using type = Primes<>; };
+
+template <size_t Head, size_t ...Tail>
+struct filter<Head, Tail...>
+{
+    using type = typename std::conditional_t<isPrime(Head),
+                               typename Cons<Head, typename filter<Tail...>::type>::type,
+                               typename filter<Tail...>::type>;
+};
+
+template<size_t... I>
+constexpr auto calc_prime(std::index_sequence<I...>) {
+    using TargetType = typename filter<I...>::type;
+    return TargetType();
+};
+
+template<size_t N>
+constexpr auto calc_prime() {
+    return calc_prime(std::make_index_sequence<N>{});
+}
+
+void test_calc_prime() {
+    constexpr auto primes = calc_prime<100>();
+    primes.print();
+}
 
 
 #endif
